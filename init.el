cb55a6d66a426fbd26e1c14bf009607b44363721
bbingju <pjhwang@gmail.com>
2026-07-23 16:55:32 +0900

    chore: silence missing-lexbind-cookie warnings from third-party files

--- init.el
+++ init.el
@@ -2,5 +2,10 @@
 ;;;
 ;;; Author: Phil Hwang <pjhwang@gmail.com>
 
+;; Silence "missing lexical-binding cookie" warnings from third-party
+;; .el files still loaded as source (ob-http, org-bullets, system mu4e, ...).
+;; Must run before those files are loaded.
+(setq warning-suppress-log-types '((files missing-lexbind-cookie)))
+
 (require 'org)
 (org-babel-load-file (concat user-emacs-directory "README.org"))
